f04c3c1c77befcf5894270df39747116ca646dac
Nathanael Allison <howlingflame39@gmail.com>
2023-05-05 15:06:30 -0400

    Start On Mapping Prototyping
    Start on mapping prototyping in order to finish up additional mechanics. Most of Link's house has been finished.

--- The Legend Down Under/Assets/Scripts/Camera/CameraMovement.cs
+++ The Legend Down Under/Assets/Scripts/Camera/CameraMovement.cs
@@ -11,27 +11,39 @@ public class CameraMovement : MonoBehaviour
     public Vector2 maxPosition;
     public Vector2 minPosition;
 
+    [Header("Animator")]
     public Animator animator;
+
+    [Header("Position Reset")]
+    public VectorValue camMin;
+    public VectorValue camMax;
 
 
     // Use this for initialization
-    void Start()
+    private void Start()
     {
+        maxPosition = camMax.initialValue;
+        minPosition = camMin.initialValue;
         animator = GetComponent<Animator>();
         transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
     }
 
-    void Update()
+    void LateUpdate()
     {
         if (transform.position != target.position)
         {
             Vector3 targetPosition = new Vector3(target.position.x,
-                                     target.position.y,
-                                     transform.position.z);
+                                                 target.position.y,
+                                                 transform.position.z);
+            targetPosition.x = Mathf.Clamp(targetPosition.x,
+                                           minPosition.x,
+                                           maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y,
+                                           minPosition.y,
+                                           maxPosition.y);
+
             transform.position = Vector3.Lerp(transform.position,
                                              targetPosition, smoothing);
-            //transform.position = Vector3.Lerp(transform.position,
-            //                                 targetPosition, smoothing);
         }
     }
 
